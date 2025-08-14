443eeff0d8fad5441773dd379494e9a174d5c146
Yash Bansal <demonstrikk@gmail.com>
2025-08-14 18:21:01 +0000

    trail fixures

--- twitterclone2.0/app/component/SessionWrapper.jsx
+++ twitterclone2.0/app/component/SessionWrapper.jsx
@@ -1,9 +1,9 @@
-"use client"; 
-import { SessionProvider } from "next-auth/react"; 
-import React from "react";
+// "use client"; 
+// import { SessionProvider } from "next-auth/react"; 
+// import React from "react";
 
-const SessionWrapper = ({ children, session }) => {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
-};
+// const SessionWrapper = ({ children, session }) => {
+//   return <SessionProvider session={session}>{children}</SessionProvider>;
+// };
 
-export default SessionWrapper;
+// export default SessionWrapper;
--- twitterclone2.0/app/layout.jsx
+++ twitterclone2.0/app/layout.jsx
@@ -1,20 +1,20 @@
 'use client';
 import './globals.css';
-import SessionWrapper from './component/SessionWrapper';
+// import SessionWrapper from './component/SessionWrapper';
 
-import { SessionProvider } from 'next-auth/react';
+// import { SessionProvider } from 'next-auth/react';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <SessionWrapper>
-      <SessionProvider refetchInterval={300}>
+        {/* <SessionWrapper> */}
+      {/* <SessionProvider refetchInterval={300}> */}
         {/* <LoadingProvider> */}
           {children}
           {/* <SplashScreen /> */}
          {/* </LoadingProvider>  */}
-  </SessionProvider>
-        </SessionWrapper>
+  {/* </SessionProvider> */}
+        {/* </SessionWrapper> */}
       </body>
     </html>
   );
