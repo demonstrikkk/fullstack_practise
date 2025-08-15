ed1b6edcd0189e270a8130bada11db4ec98b2bd5
Yash Bansal <demonstrikk@gmail.com>
2025-08-15 07:54:13 +0000

    slug  removed

--- twitterclone2.0/app/api/[slug]/route.js
+++ twitterclone2.0/app/api/[slug]/route.js
@@ -1,37 +1,37 @@
 
 
-import dbConnect from '../lib/dBconnect';
-import UserProfile from '../lib/models/UserProfile';
+// import dbConnect from '../lib/dBconnect';
+// import UserProfile from '../lib/models/UserProfile';
 
-export async function POST(req) {
-  await dbConnect();
+// export async function POST(req) {
+//   await dbConnect();
 
-  const contentType = req.headers.get("content-type");
-  let body;
+//   const contentType = req.headers.get("content-type");
+//   let body;
 
-  try {
-    if (contentType && contentType.includes("application/json")) {
-      body = await req.json();
-    } else if (contentType && contentType.includes("multipart/form-data")) {
-      const formData = await req.formData();
-      body = {};
-      formData.forEach((value, key) => {
-        body[key] = value;
-      });
-    } else {
-      return new Response(JSON.stringify({ error: "Unsupported Content-Type" }), { status: 400 });
-    }
+//   try {
+//     if (contentType && contentType.includes("application/json")) {
+//       body = await req.json();
+//     } else if (contentType && contentType.includes("multipart/form-data")) {
+//       const formData = await req.formData();
+//       body = {};
+//       formData.forEach((value, key) => {
+//         body[key] = value;
+//       });
+//     } else {
+//       return new Response(JSON.stringify({ error: "Unsupported Content-Type" }), { status: 400 });
+//     }
 
-    const pathname = req.nextUrl?.pathname;
+//     const pathname = req.nextUrl?.pathname;
 
-    if (pathname === '/api/save') {
-      const newStudent = new UserProfile(body);
-      await newStudent.save();
-      return new Response(JSON.stringify({ message: 'Data inserted', student: newStudent }), { status: 201 });
-    } else {
-      return new Response(JSON.stringify({ message: 'Invalid API Endpoint' }), { status: 404 });
-    }
-  } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
-  }
-}
+//     if (pathname === '/api/save') {
+//       const newStudent = new UserProfile(body);
+//       await newStudent.save();
+//       return new Response(JSON.stringify({ message: 'Data inserted', student: newStudent }), { status: 201 });
+//     } else {
+//       return new Response(JSON.stringify({ message: 'Invalid API Endpoint' }), { status: 404 });
+//     }
+//   } catch (error) {
+//     return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+//   }
+// }
