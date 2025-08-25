1ecab700543c3f7fd0c816743eb4664b8cd9597a
Yash Bansal <demonstrikk@gmail.com>
2025-08-25 10:32:26 +0000

    fix build 1.2.11

--- twitterclone2.0/app/api/lib/supabaseClient.js
+++ twitterclone2.0/app/api/lib/supabaseClient.js
@@ -1,8 +1,23 @@
-// lib/supabaseClient.js
+// // lib/supabaseClient.js
+// import { createClient } from '@supabase/supabase-js';
+// export const dynamic = 'force-dynamic';
+
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+
 import { createClient } from '@supabase/supabase-js';
 export const dynamic = 'force-dynamic';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true, // important for redirects
+  }
+});
--- twitterclone2.0/app/api/lib/supabaseClientbrowser.js
+++ twitterclone2.0/app/api/lib/supabaseClientbrowser.js
@@ -1,8 +1,13 @@
 "use client";
 
 import { createBrowserClient } from "@supabase/ssr";
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+export const supabase = createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true, // important for redirects
+  }
+});
